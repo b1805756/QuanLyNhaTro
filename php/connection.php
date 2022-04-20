856d4793994e78e0c0b8714a791b196e65d3eeed
b1805756 <lhieu909p@gmail.com>
2022-04-20 20:23:00 +0700

    Update connection.php

--- php/connection.php
+++ php/connection.php
@@ -4,6 +4,7 @@ $database = "qlnhatro";
 $username = "root";
 $password = "";
 // Create connection
+//Dong nay them vo choi
 $conn = mysqli_connect($servername, $username, $password, $database);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
