c6f0112c0558aa2e2743094ca5af33aab73c480d
b1805756 <lhieu909p@gmail.com>
2022-04-22 10:52:40 +0700

    Update dangkyphong.php

--- php/dangkyphong.php
+++ php/dangkyphong.php
@@ -8,11 +8,13 @@ $sodienthoai = $_POST['inputSDT'];
 $gioitinh = $_POST['GioiTinh'];
 $tenphong = $_POST['inputTenPhong'];
 $ngaychothue = $_POST['inputNgayChoThue'];
+$tentaikhoan = $_POST['inputUsername'];
+$matkhau = $_POST['inputPassword'];
 
 echo $hoten."<br>".$cccd."<br>".$ngaysinh."<br>".$quequan."<br>".$sodienthoai."<br>".$gioitinh."<br>".$tenphong."<br>".$ngaychothue;
 
 $sql = "INSERT INTO qlchothue VALUES ('".$cccd."', '".$hoten."', '".$gioitinh."', '".$ngaysinh."', '".$quequan."', '".$sodienthoai."', ".$tenphong.", '".$ngaychothue."')";
-
+$sql = "INSERT INTO qlchothue VALUES ('".$cccd."', '".$hoten."', '".$ngaysinh."', '".$quequan."', '".$sodienthoai."', '".$gioitinh."', '".$tenphong."', '".$ngaychothue."', '".$tentaikhoan."', '".$matkhau."')";
 $query = mysqli_query($conn, $sql);
 
 if($query === true){
