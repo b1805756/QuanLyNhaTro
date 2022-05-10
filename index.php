9daa92bdc9b2158d83477e37821b5077d1217426
b1805756 <lhieu909p@gmail.com>
2022-05-10 12:02:24 +0700

    1

--- index.php
+++ index.php
@@ -21,6 +21,7 @@ if(isset($_SESSION['username'])){
     <div id="wrapper">
         <form method="post" action="./php/login.php" id="form-login">
             <h1 class="form-heading">Nhà trọ TH&DH</h1>
+            <img src="./img/logo1.png" alt="" height="80%" width="80%">
             <div class="form-group">
                 <i class="far fa-user"></i>
                 <input type="text" class="form-input" name="username" placeholder="Tên đăng nhập" required>
